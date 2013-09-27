b63eab0a244422168445abceb3899a9b564c94b5
AmanKubanychbek <amankubanychbek@gmail.com>
2013-09-27 20:42:28 +0600

    deleted files
    no need in this project

--- DMT task type 1/DMT task type 1/TaskModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Ownmenot\documents\visual studio 2012\Projects\DMT task type 1\DMT task type 1\TaskModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
