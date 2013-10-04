59d87f8c9bc70b6e69436aae7f2eb2e401d9cdcc
abd256 <abd2561024@gmail.com>
2013-10-04 16:20:49 +0700

    Перенес проект на 10 студию

---
+++ DSS/DecisionSupportSystem/DecisionSupportSystem/ActionEventInputPageType1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DecisionSupportSystem
+{
+	public partial class ActionEventInputPageType1
+	{
+		public ActionEventInputPageType1()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+			// Узбакойза
+		}
+	}
+}
