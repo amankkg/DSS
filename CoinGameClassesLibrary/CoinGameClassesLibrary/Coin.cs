5612ee822d4bfa76051a12fe868337f7b5d1a5f0
AmanKubanychbek <amankubanychbek@gmail.com>
2014-01-21 22:50:41 +0600

    commented Coin.cs

--- CoinGameClassesLibrary/CoinGameClassesLibrary/Coin.cs
+++ CoinGameClassesLibrary/CoinGameClassesLibrary/Coin.cs
@@ -8,29 +8,28 @@ namespace CoinGameClassesLibrary
         int _NumberOfInitialEvents;
         int _NumberOfThrowings;
 
-        public List<int[]> Outcomes; //  исходы (события)
+        public List<int[]> Outcomes; //  исходы (события), где каждый исход - массив индексов исходных событий
 
         int[] currentIndexes;   // temp var for recursion
         void GenerateOutcomes(int throwing = 1)
-        {
+        {//рекурсивная функция, параметр - номер текущего броска, по умолчанию = 1
             if (throwing == 1)
             {
-                currentIndexes = new int[_NumberOfThrowings];
-                Outcomes = new List<int[]>();
-                //size = Convert.ToInt32(Math.Pow(InitialEvents.Length, NumberOfThrowings))
+                currentIndexes = new int[_NumberOfThrowings];   //создается временный массив для индексов символов исходных событий
+                Outcomes = new List<int[]>();   //создается список исходов
             }
             for (int i = 0; i < _NumberOfInitialEvents; i++)
-            {
-                currentIndexes[throwing - 1] = i;
+            {//цикл для обхода исходных событий
+                currentIndexes[throwing - 1] = i;   //временный массив сохраняет индекс исхода на позиции, равной текущему броску, т.к. индексация массива с нуля, то throwing - 1
                 if (throwing < _NumberOfThrowings)
-                {   //  переход к следующему броску
-                    GenerateOutcomes(throwing + 1);
+                {   //  переход к следующему броску если текущий бросок не последний, например, 1-ый или 2-ой из 3-ех
+                    GenerateOutcomes(throwing + 1); //рекурсивный вызов, куда передается номер следующего броска
                 }
                 else
-                {   //  добавление исхода
-                    int[] item = new int[_NumberOfThrowings];
-                    Array.Copy(currentIndexes, item, _NumberOfThrowings);
-                    Outcomes.Add(item);
+                {   //  иначе, если бросок последний, например 3-ий из 3-ех, добавление исхода
+                    int[] item = new int[_NumberOfThrowings];   //временный массив для передачи индексов символов названия исхода
+                    Array.Copy(currentIndexes, item, _NumberOfThrowings);   //копирование временного массива с именем в новый массив
+                    Outcomes.Add(item); //добавление исхода в список
                 }
             }
         }
@@ -39,18 +38,18 @@ namespace CoinGameClassesLibrary
         {   //  NumberOfEventForSequence - учитываемый для последовательности исходный выпад
             //  NumberOfInitialEventsInSequence - учитываемое количество событий в последовательности (по умолчанию = 1)
             //  при значении по умолчанию, получаем просто количетсво, иначе считаем подряд выпавшие события
-            List<int> resultList = new List<int>();
+            List<int> resultList = new List<int>(); //список результатов поиска последовательности по исходам
             for (int i = 0; i < Outcomes.Count; i++)
-            {
-                int result = 0;
-                bool prevMatches = false;
-                int countMatches = 0;   //  количество выпадений нужного исхода подряд
+            {//цикл обходит все исходы
+                int result = 0; //обнуляем счетчик для подсчета количества искомой последовательности при текущем исходе
+                bool prevMatches = false;   //обнуляем флаг для начала последовательности (не играет роли при последовательности в длину 1)
+                int countMatches = 0;   //  обнуляем количество выпадений нужного исхода подряд
                 for (int j = 0; j < Outcomes[i].Length; j++)
-                {
+                {//цикл обходит индексы текущего исхода
                     if (Outcomes[i][j] == NumberOfEventForSequence)
                     {
                         //  если искомый выпад найден
-                        countMatches++;
+                        countMatches++; //счетчик инкременирует
                         if (NumberOfInitialEventsInSequence == 1 || prevMatches)
                         {   //  если предыдущий выпад был искомым
                             if (countMatches >= NumberOfInitialEventsInSequence)
@@ -59,19 +58,19 @@ namespace CoinGameClassesLibrary
                             }
                         }
                         else
-                        {
-                            prevMatches = true;
+                        {   // иначе, если выпад был искомый, но не достаточной длины, то
+                            prevMatches = true; //указываем, что последовательность началась
                         }
                     }
                     else
                     {
                         //  если искомый выпад не найден
-                        countMatches = 0;
+                        countMatches = 0;   //обнуляем последовательность
                     }
                 }
-                resultList.Add(result);
+                resultList.Add(result); //добавляем результат поиска по данному исходу
             }
-            return resultList;
+            return resultList;  //результат работы функции
         }
 
         public Coin(int NumberOfInitialEvents, int NumberOfThrowings)
